4140932d8e77ebbea2c4e00f9b9a31d67b57b4bd
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-03-24 21:56:57 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,13 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
+describe "./variable.rb" do
 
+  it "defined a local variable called greeting and set it equal to 'Hello World'" do
+    greeting = get_variable_from_file('./variable.rb', "greeting")
+
+    expect(greeting).to eq("Hello World")
+  end
+
+end
 
