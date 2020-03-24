a93fa9193fd6c50ef537b74e97faedf718e46f9c
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-03-24 21:58:14 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,30 +1,3 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-
-require_relative '../variable.rb'
-
-RSpec.configure do |config|
-  config.order = 'default'
-end
-
-def get_variable_from_file(file, variable)
-  file_scope = binding
-  file_scope.eval(File.read(file))
-
-  begin
-    return file_scope.local_variable_get(variable)
-  rescue NameError
-    raise NameError, "local variable `#{variable}` not defined in #{file}."
-  end
-end
-
-describe "./variable.rb" do
-
-  it "defined a local variable called greeting and set it equal to 'Hello World'" do
-    greeting = get_variable_from_file('./variable.rb', "greeting")
-
-    expect(greeting).to eq("Hello World")
-  end
-
-end
-
+def greeting
+  puts "Helllo World"
+  
